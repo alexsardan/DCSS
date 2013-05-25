6cc962f366ed01f8dcb7b638a2d53249a24834e7
Ioan Balas <ioanbalas@outlook.com>
2013-05-25 13:01:24 +0300

    + Generic Library (Response, Reply, UploadFile)

--- DCSS-Client/src/dcss/client/DCSSClient.java
+++ DCSS-Client/src/dcss/client/DCSSClient.java
@@ -7,8 +7,11 @@ package dcss.client;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import my.generic.lib.*;
 
 /**
  *
@@ -16,24 +19,90 @@ import java.util.logging.Logger;
  */
 public class DCSSClient {
 
+    public static String processRequest(String req)
+    {
+        StringTokenizer st = new StringTokenizer(req);
+        
+        if (st.countTokens() < 1)
+            return "Numar insificient de parametri";
+        
+        String command = st.nextToken();
+        
+        switch(command)
+        {
+            case "create":
+                if (st.countTokens() == 2)
+                {
+                    String name = st.nextToken();
+                    String pass = st.nextToken();
+                    
+                    break;
+                }
+                else
+                    return "Apel incorect! \n client [nume] [parola] \n";
+            case "login":
+                if (st.countTokens() == 2)
+                {
+                    String name = st.nextToken();
+                    String pass = st.nextToken();
+                    
+                    break;
+                }
+                return "Apel incorect! \n login [nume] [parola] \n";
+                
+            case "list":
+                if (st.countTokens() > 0)
+                    return "Comanda 'list' nu necesita parametri suplimentari";
+                else
+                    ;                    
+                break;
+            case "download":
+                if (st.countTokens() == 2)
+                {
+                    try
+                    {
+                        int id = Integer.parseInt(st.nextToken());
+                    } catch(NumberFormatException ex)
+                    {
+                        return "Parametrul [id_fisier] in format incorect";
+                    }
+                    
+                    String name = st.nextToken();
+                    break;
+                }
+                
+            case "upload":
+                if (st.countTokens() == 3)
+                {
+                    
+                }
+                break;
+            default:
+                return "Comanda incorecta! [create, login, list, download, upload]";
+        }       
+        
+        return "";
+    }
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         try {
             BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
             String command;
-            
+            //
             System.out.print("> ");
-            while ((command = console.readLine()) != null) {
+            while ((command = console.readLine()) != null) 
+            {
                 if (command.equals("exit")) {
                     return;
                 } else {
-                    System.out.println(command);
+                   System.out.println(processRequest(command));
                 }
                 System.out.print("> ");
             }
-        } catch (IOException ex) {
+        } catch (Exception ex) {
             Logger.getLogger(DCSSClient.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
