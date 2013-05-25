c5a78867d9dea141a251d827e94673c0ea150d79
Ioan Balas <ioanbalas@outlook.com>
2013-05-25 19:15:31 +0300

    * GenericLibrary 	+ User class

---
+++ GenericLibrary/src/my/generic/lib/User.java
@@ -0,0 +1,14 @@
+package my.generic.lib;
+
+public class User 
+{
+    
+    public String name;
+    public String password;
+    
+    public User(String name, String password)
+    {
+        this.name = name;
+        this.password = password;
+    }    
+}
